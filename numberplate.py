5e6ba4cdf4582a270a18cbaa2742c5d50b4f7484
bunny6 <43778618+bunny6@users.noreply.github.com>
2022-05-12 19:13:24 +0530

    Add files via upload

---
+++ numberplate.py
@@ -0,0 +1,18 @@
+
+#Cloneing yolov5
+!git clone https://github.com/ultralytics/yolov5  # clone
+%cd yolov5
+%pip install -qr requirements.txt  # install
+
+#importing pytorch
+import torch
+from yolov5 import utils
+display = utils.notebook_init()  # checks
+
+!unzip -q ../train_data.zip -d../ #our file was a zip file
+
+#Train YOLOv5s on COCO128 for 15 epochs
+!python train.py --img 640 --batch 30 --epochs 20 --data coco128.yaml --weights yolov5s.pt --cache
+
+#detecting
+!python detect.py --weights runs/train/exp/weights/best.pt --img 640 --conf 0.25 --source /content/city.jpeg --save-crop
