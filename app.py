4ee992b221d977979dfd9e9bcfb04d933c0680e9
bunny6 <43778618+bunny6@users.noreply.github.com>
2022-05-18 18:51:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,43 +1,41 @@
+#Importing libraries
 import torch
 import io
 from PIL import Image
 import cv2
 import numpy as np
-
+import imutils
 import os
 from flask import Flask, render_template, request, redirect, url_for, flash
 from itsdangerous import Signer, BadSignature, want_bytes
-import psycopg2 #pip install psycopg2 
+import psycopg2 #installing postgresql
 import psycopg2.extras
-import pytesseract
-# conn = psycopg2.connect( #psycopg2 database adaptor for implementing python
-#         host="localhost",
-#         database="students",
-#         user='postgres',
-#         password='p@ssw0rd')
+import pytesseract #extraction of text from image using tesseract
+
+conn = psycopg2.connect( #psycopg2 database adaptor for implementing python
+        host="localhost",
+        database="num_plate",
+        user='postgres',
+        password='p@ssw0rd')
 
 app = Flask(__name__)
 
-RESULT_FOLDER = os.path.join('static')
-app.config['RESULT_FOLDER'] = RESULT_FOLDER
-
+#yolov5 model with the trained weights of dataset
 model = torch.hub.load( '/home/neosoft/Documents/ocr/yolov5','custom',path='/home/neosoft/Documents/ocr/best.pt',source='local') # for PIL/cv2/np inputs and
 
+#for detecting the number plate
 def get_prediction(img_bytes):
     img = Image.open(io.BytesIO(img_bytes))
-     # batched list of images
-
-# Inference
-    results = model(img, size=640)  # includes NMS
+    results = model(img, size=640)  
     return results
 
-
+#flask home page 
 @app.route('/', methods=['GET', 'POST'])
 def predict():
     if request.method == 'POST':
         if 'file' not in request.files:
             return redirect(request.url)
-        file = request.files.get('file')
+        file = request.files.get('file') #to take i/p(image) from the user
         if not file:
             return
 
@@ -45,20 +43,46 @@ def predict():
         results = get_prediction(img_bytes)
         print(type(results))
         results.save('results0.jpg')
-       
-        crops = results.crop(save=True)  
-        #cv2.imshow('croped',crops[0]['im'])
-        crops=crops[0]['im']
-        #array1=np.array(crops)
-        #var1=array1.astype(np.uint8)
-        img=Image.fromarray(crops)
+    
+        crops = results.crop(save=True) #cropping the image on bounding box
+        crops=crops[0]['im'] #to get the image array from the array created from above step
+        img = imutils.resize(crops, width=300 )
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #to convert image to gray
+        img = cv2.resize(img, None, fx=2, fy=2, interpolation=cv2.INTER_CUBIC)
+        
+        img = cv2.bilateralFilter(img, 11, 17, 17) 
+        img = cv2.medianBlur(img, 3)
+        #cv2.threshold(img,127,255,cv2.THRESH_BINARY)
+        cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 31, 2)
+        
+        img=Image.fromarray(img) #to get image from array
+        img.show()#to display image
+        text = pytesseract.image_to_string(img)#to extract the charcters from number plate
+        
+        #for removing empty spaces
+        text1='' 
+        for i in text:
+            if i==" ":
+                continue
+            else:
+                text1+=i
+        text2=''        
+        for i in text1:
+            if i.lower()!=i or i.isdigit():
+                text2+=i        
+        
+        cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+        s = "SELECT * FROM VEHICLE where vehicle_number =%s"
 
-        img.show()
-        text = pytesseract.image_to_string(img)
-        return render_template('results.html',path=text)   
+        cur.execute(s, (text2,))
+        
+        res = cur.fetchall()
+        if len(res)==1:
+            return render_template('results.html',path=text2,msg="Access granted for your car.")#to redirect to results html page   
 
-       
-     
+        else:
+            return render_template('results.html',path=text2,msg="Access not granted for your car.")#to redirect to results html page   
+
     return render_template('index.html')    
 app.secret_key = 'the random string' 
 if __name__ == "__main__":
